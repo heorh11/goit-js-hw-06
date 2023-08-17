04490fcf7f0dfc3b52bfebfe0a17be60a0742677
heorh11 <gheorghe.filip225@gmail.com>
2023-08-17 21:13:29 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,8 @@
 const listOfCategories = document.querySelectorAll(".item")
 console.log(`Number of categories: ${listOfCategories.length}`);
-listOfCategories.forEach(item => {
+
    listOfCategories.forEach(category => {
     console.log(`Category: ${category.firstElementChild.textContent}`);
        console.log(`Elements: ${category.querySelectorAll("li").length}`);
        
 });
-})
