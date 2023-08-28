b64f0bbe5ffe743e135e5ca90677c4ac790a85d2
heorh11 <gheorghe.filip225@gmail.com>
2023-08-28 22:24:48 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,26 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBoxBtn = document.querySelector('[data-create]');
+const destroyBoxBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+createBoxBtn.addEventListener('click', () => {
+  const amount = +document.querySelector('input').value;
+  const boxesArray = [];
+  
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesArray.push(box);
+  }
+
+  boxesContainer.append(...boxesArray);
+});
+
+destroyBoxBtn.addEventListener('click', () => {
+  boxesContainer.innerHTML = '';
+});
